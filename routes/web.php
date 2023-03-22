513dd1cef574451044a69d2916c5720ac031cfa6
rohitsasi720 <rohitsasi720@gmail.com>
2023-03-22 10:03:47 +0530

    updated

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,13 @@
 <?php
 
+use App\Http\Controllers\FormController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
-Route::post('/submit-form', 'FormController@store')->name('submit-form');
+Route::post('/submit-form', [FormController::class, 'store'])->name('submit-form');
 
 
 require __DIR__.'/auth.php';
